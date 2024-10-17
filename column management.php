1eaa30d7c86233a3d7548be8a19348beda4ff6ec
Your Name <omit8998@gmail.com>
2024-10-18 00:14:01 +0600

    Update code

--- column management.php
+++ column management.php
@@ -52,21 +52,21 @@ function post_string_sortable_function($colums){
 return $colums;
 }
 add_filter('manaage_edit-post_sortable_columns','post_string_sortable_function');
-
-// function coldemo_set_word_count(){
-//     $posts = get_posts(array(
-//         'posts_per_page'=>-1,
-//         'post_type'=>'post',
-//            'post_status' => 'any'
-//     ));
-//     foreach($post as $p){
-//         $content = $p->post_content;
-//         $wordn = str_word_count(strip_tags($content));
-//         update_post_meta($p->ID,'wordn',$wordn);
-//     }
-// }
-// add_action('init','coldemo_set_word_count');
-
+//
+function coldemo_set_word_count(){
+    $posts = get_posts(array(
+        'posts_per_page'=>-1,
+        'post_type'=>'post',
+           'post_status' => 'any'
+    ));
+    foreach($post as $p){
+        $content = $p->post_content;
+        $wordn = str_word_count(strip_tags($content));
+        update_post_meta($p->ID,'wordn',$wordn);
+    }
+}
+add_action('init','coldemo_set_word_count');
+//
 
 
 
@@ -78,6 +78,7 @@ function coldemo_sort_column_data($wpquery){
     if('wordn' == $orderby){
         $wpquery->set('meta_key','wordn');
         $wpquery->set('orderby','meta_value_num');
+        // $wpquery=>set('orderby','meta_value_num');
     }
 
 }
@@ -87,6 +88,9 @@ function save_post_word_count($post_id){
     $content = $p->post_content;
     $wordn = str_word_count(strip_tags($content));
     update_post_meta($p->ID,'wordn',$wordn);
+    // $content = $p->post_content;
+    // $wordn = str_word_count(strip_tags($content));
+    // update_post_meta($p->ID,'wordn',$wordn);
 }
 add_action('save_post','save_post_word_count');
 
@@ -99,7 +103,6 @@ function cm_filter(){
             <option value="0">Select status</option>
             <option value="0">Some Posts</option>
             <option value="0">Select posts++</option>
-
         </select>
     <?php
 }
@@ -110,7 +113,7 @@ function filter_data(){
     }
     $filter_value = isset($_GET['demofilter']) ? $_GET['demofilter'] : '';
     if('1' == $filter_value){
-        $wpquery->set('post_in',array(12,1));
+        $wpquery->set('post__in',array(19));
     }
 }
 add_action('pre_get_posts','filter_data');
